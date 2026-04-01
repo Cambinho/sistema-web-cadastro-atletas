ec7826a6ae658d941cf4c067d290c2d86a77c54d
Carlos Martins <camb1994@gmail.com>
2026-03-31 22:34:36 -0300

    adicionando variaveis de ambiente para conexao com banco

--- models/conexao.py
+++ models/conexao.py
@@ -1,13 +1,17 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class ConexaoDB:
     def __init__(self):
         self.config = {
-            "dbname": "biblioteca_db",
-            "user": "postgres",
-            "password": "Projetos123",
-            "host": "localhost",
-            "port": "5432"
+            "dbname": os.getenv("DB_NAME"),
+            "user": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "host": os.getenv("DB_HOST"),
+            "port": os.getenv("DB_PORT")
         }
 
     def conectar(self):
